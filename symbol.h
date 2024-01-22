1ff27e3b3635faf9da9202e04f0770837c3960bb
leesugil <sugilmath@gmail.com>
2024-01-22 10:44:35 -0500

    total revision required, see issues

--- symbol.h
+++ symbol.h
@@ -10,8 +10,8 @@
 struct symbol {
 	char *name;
 	double value;			/* complex to be added later */
-	unsigned int power;		/* for displaying power correctly */
-	int expr;				/* 0: LHS, 1: RHS, -1: neither */
+	int power;				/* for displaying power correctly */
+	int expr;				/* 0: unassigned 1: LHS, 2: RHS */
 	struct symbol *left;	/* for binary tree */
 	struct symbol *right;	/* for binary tree */
 };
@@ -32,7 +32,7 @@ SYMB *addSymb(SYMB *p, char *name, double value)
 		p->name = strdup(name);
 		p->value = value;
 		p->power = 1;
-		p->expr = -1;
+		p->expr = 0;
 		p->left = NULL;
 		p->right = NULL;
 	} else if ((cond = strcmp(name, p->name)) == 0) {
@@ -99,8 +99,9 @@ SYMB *parseExpr(SYMB *p, char *line, int expr)
 	 * power = 1 and expr = 0 (LHS) by default.
 	 * how do we know if this new variable is
 	 * meant to be power = 1 and expr = 1 from the beginning?
-	 * solution: make the default expr value to be -1
-	 * for not having been assigned to either side. */
+	 * solution: make the default expr value to be 0
+	 * for not having been assigned to either side.
+	 * 1: LHS, 2: RHS */
 	/* example: G M m r^-23 */
 	/* note that getword would consider r^-2.3 as six words */
 	/* use getwordg with crit = ismathexpr to take it as
@@ -108,15 +109,36 @@ SYMB *parseExpr(SYMB *p, char *line, int expr)
 	int max = strlen(line);
 	char c, name[max];
 	SYMB *ptr = NULL;
+	struct {
+		unsigned int ispower : 1;
+	} flags;
+
 	while ((c = sgetwordg(&line, name, max, ismathexpr)) != '\0') {
 		/* add a way to flag ^ */
+		if (strcomp(name, "^") == 0)
+			flags.ispower = 1;
 		/* work to be continued from here */
-		if (ptr = lookUpTree(p, name)) {
-			/* update symb */
-		} else {
-			/* add symb */
+		if (flags.ispower == 0) {
+			if ((ptr = lookUpTree(p, name)) != NULL) {
+				/* update symb */
+			} else {
+				/* add symb */
+				p = addSymb(p, name, NAN);
+			}
+		} else if (strcomp(name, "^") != 0) {
+			/* update symb power only */
+			/* this accepts symbolic power as well */
+			/* but then i'll have to re-design the structure,
+			 * power not necessarily being an integer. */
+			/* and then it's not the innate property of the variable */
+			/* maybe symbolic definition and its entry in mathematical expression should be separated */
+			/* maybe that's why it's been traditionally important to classify
+			 * unary, binary, termary operations. */
+			flags.ispower = 0;
 		}
 	}
+
+	return p;
 }
 
 SYMB *parseSymb(SYMB *p, char *line)
@@ -145,8 +167,7 @@ SYMB *parseSymb(SYMB *p, char *line)
 		token = strtok(line, delimiters);
 		int expr = 0;
 		while (token != NULL) {
-			p = parseExpr(p, token, expr);
-			expr++;
+			p = parseExpr(p, token, ++expr);
 		}
 		return p;
 	}
